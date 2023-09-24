dfc5d73448057817addbf05ce15f5dc74ea523cf
Ethan Moynihan <ethanmoynihan1@gmail.com>
2023-09-24 17:11:04 -0400

    Making the board baby

--- agent.py
+++ agent.py
@@ -1,5 +1,6 @@
 from Edge import Edge
-import Box 
+from Box import Box 
+from board import Board
 import os.path
 import sys
 import time
@@ -36,111 +37,26 @@ class Agent:
         elif oppMove[0] == oppMove[2] and 1+oppMove[1] == oppMove[3]:
             if edgeV[oppMove[0] +oppMove[1]*10].captured ==False:
                 return True
+            else: 
+                print("Move is attempting to capture an edge that has already been claimed")
+                return False
         elif oppMove[1] == oppMove[3] and 1+oppMove[0] == oppMove[1]:
             if edgeH[oppMove[0] +oppMove[1]*10].captured ==False:
                 return True
+            else:
+                print("Move is attempting to capture an edge that has already been claimed")
+                return False
         else:
-            print("Move is attempting to capture an edge that has already been claimed")
+            print("Move is attempting to connect two points that are not adjacent")
             return False
-        pass
 
     def check_filler_move(move):
         if move[0] == 0 and move[1] == 0 and move[2] == 0 and move[3] == 0:
             return True
         else:
             return False
-
-    def update_edge(self, move, team):
-        # Vertical
-        if move[0] == move[2] and 1 + move[1] == move[3]:
-            #update in the vertical edge array
-            edgeV[move[0] + move[1]* 9].setCaptured()
-
-            if(move[0] == 0):#edge case of left side
-                # update the value of the box on the right 
-                board[move[0] + move[1] * 8].setNumEdges()
-                if board[move[0] + move[1] * 8].totalEdges == 4:
-                    board[move[0] + move[1] * 8].setCaptured()
-                    board[move[0] + move[1] * 8].setTeam(team)
-                    if team:
-                        ply+=1
-                    else:
-                        opp+=1
-            elif move[0] == 9:#edge case of the right side
-            #update the value of the box on the left
-                board[move[0] + move[1] * 8 - 1].setNumEdges()
-                if board[move[0] + move[1] * 8 - 1].totalEdges == 4:
-                    board[move[0] + move[1] * 8 - 1].setCaptured()
-                    board[move[0] + move[1] * 8 - 1].setTeam(team)
-                    if team:
-                        ply+=1
-                    else:
-                        opp+=1
-            else:
-                # update the value of the box on the right
-                board[move[0] + move[1] * 8].setNumEdges()
-                #check if captured
-                if board[move[0] + move[1] * 8].totalEdges == 4:
-                    board[move[0] + move[1] * 8].setCaptured()
-                    board[move[0] + move[1] * 8].setTeam(team)
-                    if team:
-                        ply+=1
-                    else:
-                        opp+=1
-                # update the value of the box on the left
-                board[move[0] + move[1] * 8 - 1].setNumEdges()
-                #check if captured
-                if board[move[0] + move[1] * 8 - 1].totalEdges == 4:
-                    board[move[0] + move[1] * 8 - 1].setCaptured()
-                    board[move[0] + move[1] * 8 - 1].setTeam(team)
-                    if team:
-                        ply+=1
-                    else:
-                        opp+=1
-        # Horizontal        
-        elif move[1] == move[3] and move[0] == 1 + move[2]:
-            edgeH[move[0] + move[1]* 10].setCaptured()
-            #updates amount of edges box has
-            if move[1] == 0:
-                board[move[0] + move[1] * 8].setNumEdges()
-                #if box on the bottom was captured
-                if board[move[0] + move[1] * 8].totalEdges == 4:
-                    board[move[0] + move[1] * 8].setCaptured()
-                    board[move[0] + move[1] * 8].setTeam(team)
-                    if team:
-                        ply+=1
-                    else:
-                        opp+=1
-            elif move[1] == 9:
-                board[move[0] + move[1] * 8 - 1].setNumEdges()
-                #//if box on the top was captured
-                if(board[move[0] + move[1] * 8 - 1].totalEdges == 4):
-                    board[move[0] + move[1] * 8 - 1].setCaptured()
-                    board[move[0] + move[1] * 8 - 1].setTeam(team)
-                    if team:
-                        ply+=1
-                    else:
-                        opp+=1
-            else:
-                board[move[0] + move[1] * 8].setNumEdges()
-                #//if box on the bottom was captured
-                if board[move[0] + move[1] * 8].totalEdges == 4:
-                    board[move[0] + move[1] * 8].setCaptured()
-                    board[move[0] + move[1] * 8].setTeam(team)
-                    if team:
-                        ply+=1
-                    else:
-                        opp+=1
-                board[move[0] + move[1] * 8 - 1].setNumEdges(board[move[0] + move[1] * 8- 1].totalEdges + 1)
-                #//if box on the top was captured
-                if board[move[0] + move[1] * 8 - 1].totalEdges == 4:
-                    board[move[0] + move[1] * 8 - 1].setCaptured()
-                    board[move[0] + move[1] * 8 - 1].setTeam(team)
-                    if team:
-                        ply+=1
-                    else:
-                        opp+=1
-                  
+          
+    # reads the move from move_file.txt and puts the move into oppMove[]      
     def read_move(self):
         file_contents = ''
         with open('move_file.txt', 'r') as file:
@@ -169,14 +85,12 @@ class Agent:
                 oppMove[2] =  tempx
                 oppMove[3] =  tempy
 
-    def check_win(self):
-        # Your check win logic here
-        if ply + opp == 81:
-            return True
-        else:
-            return False
+    # overwrites the old move in move_file.txt with the new move
+    def write_move(self):
+        new_move = open("move_file.txt", "w")
+        new_move.write(self.playername, " ", currMove[0], ",", currMove[1])
         
-    def makeMove (self):
+    def makeMove (self,):
         bothEdgeList = edgeV + edgeH
         if len(bothEdgeList) < 1:
             #no moves left
@@ -236,26 +150,11 @@ class Agent:
 
 def main():
     agent = Agent()
+    gameBoard = Board()
     currTime = 0
     startTime = 0
-
-    edgeV = [Edge(0, 0, 0, 0) for _ in range(90)]
-    edgeH = [Edge(0, 0, 0, 0) for _ in range(90)]
-    board = [Box(Edge(0, 0, 0, 0), Edge(0, 0, 0, 0), Edge(0, 0, 0, 0), Edge(0, 0, 0, 0)) for _ in range(81)]
-
-    for y in range(9):
-        for x in range(10):
-            edgeV[x + y * 10] = Edge(x, y, x, y + 1)
-
-    for y in range(10):
-        for x in range(9):
-            edgeH[x + y * 9] = Edge(x, y, x + 1, y)
-
-    for y in range(9):
-        for x in range(9):
-            board[x + y * 8] = Box(edgeH[x + y * 8], edgeV[x + y * 8 + 1], edgeV[x + y * 8], edgeH[x + y * 8 + 1])
     
-    while(agent.check_win is not True):
+    while(gameBoard.check_win is not True):
         #while the move_file.txt file does not exist the program is supposed to wait until it does exist
         while((not os.path.exists("./groupname.go")) or (not os.path.exists("./groupname.pass"))):
             pass
@@ -276,7 +175,8 @@ def main():
         while(time.time() - start < TIME_LIMIT):
             pass #TODO calculate our next move should happen here
 
-        #TODO write the new move to move_file.txt
+        #writes currMove to move_file.txt
+        agent.write_move()
     
     #TODO Declare winner:
 
--- board.py
+++ board.py
@@ -3,113 +3,87 @@ from Box import *
 
 class Board:
     def __init__(self):
-        edgeV = [Edge(0, 0, 0, 0) for _ in range(90)]
-        edgeH = [Edge(0, 0, 0, 0) for _ in range(90)]
-        board = [Box(Edge(0, 0, 0, 0), Edge(0, 0, 0, 0), Edge(0, 0, 0, 0), Edge(0, 0, 0, 0)) for _ in range(81)]
+        self.edgeV = [Edge(0, 0, 0, 0) for _ in range(90)]
+        self.edgeH = [Edge(0, 0, 0, 0) for _ in range(90)]
+        self.board = [Box(Edge(0, 0, 0, 0), Edge(0, 0, 0, 0), Edge(0, 0, 0, 0), Edge(0, 0, 0, 0)) for _ in range(81)]
 
         for y in range(9):
             for x in range(10):
-                edgeV[x + y * 10] = Edge(x, y, x, y + 1)
+                self.edgeV[x + y * 10] = Edge(x, y, x, y + 1)
 
         for y in range(10):
             for x in range(9):
-                edgeH[x + y * 9] = Edge(x, y, x + 1, y)
+                self.edgeH[x + y * 9] = Edge(x, y, x + 1, y)
 
         for y in range(9):
             for x in range(9):
-                board[x + y * 8] = Box(edgeH[x + y * 8], edgeV[x + y * 8 + 1], edgeV[x + y * 8], edgeH[x + y * 8 + 1])
+                self.board[x + y * 8] = Box(self.dgeH[x + y * 8], self.edgeV[x + y * 8 + 1], self.edgeV[x + y * 8], self.edgeH[x + y * 8 + 1])
         self.ply = 0
         self.opp = 0
+        
+        def check_cap(self, num, team):
+            if self.board[num].totalEdges == 4:
+                self.board[num].setCaptured()
+                self.board[num].setTeam(team)
+                if team:
+                    self.ply+=1
+                else:
+                    self.opp+=1
+
         def update_edge(self, move, team):
         # Vertical
             if move[0] == move[2] and 1 + move[1] == move[3]:
                 #update in the vertical edge array
-                edgeV[move[0] + move[1]* 9].setCaptured()
+                self.edgeV[move[0] + move[1]* 9].setCaptured()
 
                 if(move[0] == 0):#edge case of left side
                     # update the value of the box on the right 
-                    board[move[0] + move[1] * 8].setNumEdges()
-                    if board[move[0] + move[1] * 8].totalEdges == 4:
-                        board[move[0] + move[1] * 8].setCaptured()
-                        board[move[0] + move[1] * 8].setTeam(team)
-                        if team:
-                            ply+=1
-                        else:
-                            opp+=1
+                    self.board[move[0] + move[1] * 8].setNumEdges()
+                    self.check_cap(move[0] + move[1] * 8, team)
                 elif move[0] == 9:#edge case of the right side
                 #update the value of the box on the left
-                    board[move[0] + move[1] * 8 - 1].setNumEdges()
-                    if board[move[0] + move[1] * 8 - 1].totalEdges == 4:
-                        board[move[0] + move[1] * 8 - 1].setCaptured()
-                        board[move[0] + move[1] * 8 - 1].setTeam(team)
-                        if team:
-                            ply+=1
-                        else:
-                            opp+=1
+                    self.board[move[0] + move[1] * 8 - 1].setNumEdges()
+                    self.check_cap(move[0] + move[1] * 8 - 1, team)
                 else:
                     # update the value of the box on the right
-                    board[move[0] + move[1] * 8].setNumEdges()
+                    self.board[move[0] + move[1] * 8].setNumEdges()
                     #check if captured
-                    if board[move[0] + move[1] * 8].totalEdges == 4:
-                        board[move[0] + move[1] * 8].setCaptured()
-                        board[move[0] + move[1] * 8].setTeam(team)
-                        if team:
-                            ply+=1
-                        else:
-                            opp+=1
+                    self.check_cap(move[0] + move[1] * 8, team)
                     # update the value of the box on the left
-                    board[move[0] + move[1] * 8 - 1].setNumEdges()
+                    self.board[move[0] + move[1] * 8 - 1].setNumEdges()
                     #check if captured
-                    if board[move[0] + move[1] * 8 - 1].totalEdges == 4:
-                        board[move[0] + move[1] * 8 - 1].setCaptured()
-                        board[move[0] + move[1] * 8 - 1].setTeam(team)
-                        if team:
-                            ply+=1
-                        else:
-                            opp+=1
+                    self.check_cap(move[0] + move[1] * 8 - 1, team)
             # Horizontal        
             elif move[1] == move[3] and move[0] == 1 + move[2]:
-                edgeH[move[0] + move[1]* 10].setCaptured()
+                self.edgeH[move[0] + move[1]* 10].setCaptured()
                 #updates amount of edges box has
                 if move[1] == 0:
-                    board[move[0] + move[1] * 8].setNumEdges()
+                    self.board[move[0] + move[1] * 8].setNumEdges()
                     #if box on the bottom was captured
-                    if board[move[0] + move[1] * 8].totalEdges == 4:
-                        board[move[0] + move[1] * 8].setCaptured()
-                        board[move[0] + move[1] * 8].setTeam(team)
-                        if team:
-                            ply+=1
-                        else:
-                            opp+=1
-                elif move[1] == 9:
-                    board[move[0] + move[1] * 8 - 1].setNumEdges()
-                    #//if box on the top was captured
-                    if(board[move[0] + move[1] * 8 - 1].totalEdges == 4):
-                        board[move[0] + move[1] * 8 - 1].setCaptured()
-                        board[move[0] + move[1] * 8 - 1].setTeam(team)
-                        if team:
-                            ply+=1
-                        else:
-                            opp+=1
-                else:
-                    board[move[0] + move[1] * 8].setNumEdges()
-                    #//if box on the bottom was captured
-                    if board[move[0] + move[1] * 8].totalEdges == 4:
-                        board[move[0] + move[1] * 8].setCaptured()
-                        board[move[0] + move[1] * 8].setTeam(team)
-                        if team:
-                            ply+=1
-                        else:
-                            opp+=1
-                    board[move[0] + move[1] * 8 - 1].setNumEdges(board[move[0] + move[1] * 8- 1].totalEdges + 1)
-                    #//if box on the top was captured
-                    if board[move[0] + move[1] * 8 - 1].totalEdges == 4:
-                        board[move[0] + move[1] * 8 - 1].setCaptured()
-                        board[move[0] + move[1] * 8 - 1].setTeam(team)
-                        if team:
-                            ply+=1
-                        else:
-                            opp+=1
-            
+                    self.check_cap(move[0] + move[1] * 8, team)
 
-        
+                elif move[1] == 9:
+                    self.board[move[0] + move[1] * 8 - 1].setNumEdges()
+                    #//if box on the top was captured
+                    self.check_cap(move[0] + move[1] * 8 - 1, team)
+
+                else:
+                    # set edges in first box
+                    self.board[move[0] + move[1] * 8].setNumEdges()
+                    #//if box on the bottom was captured
+                    self.check_cap(move[0] + move[1] * 8, team)
+                    
+                    # set edges in second box
+                    self.board[move[0] + move[1] * 8 - 1].setNumEdges(self.board[move[0] + move[1] * 8- 1].totalEdges + 1)
+                    
+                    #//if box on the top was captured
+                    self.check_cap(move[0] + move[1] * 8 - 1, team)
+                    
+
+
+        def check_win(self):
+            # Your check win logic here
+            if self.ply + self.opp == 81:
+                return True
+            else:
+                return False
